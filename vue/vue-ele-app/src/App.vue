199b8c991ea152aedcd48fcb21f26c9364ba398f
zph970910 <1076299371@qq.com>
2019-11-01 19:42:25 +0800

    11.1

---
+++ vue/vue-ele-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <v-header></v-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/header.vue'
+export default {
+  name: 'App',
+  components: {
+    'v-header' : header
+  }
+}
+</script>
+
+<style>
+
+</style>
+
