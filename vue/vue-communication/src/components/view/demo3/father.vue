5ab6410cdb4af58abf3a161ce6020fdbd8de8268
zph970910 <1076299371@qq.com>
2019-11-14 21:46:53 +0800

    父组件给子组件发送数据

---
+++ vue/vue-communication/src/components/view/demo3/father.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h2>父组件</h2>
+    <p>
+      从子组件获取的消息为:
+      <span class="red">{{Msg}}</span>
+    </p>
+
+    <child v-on:message="getMsg"></child>
+  </div>
+</template>
+
+<script>
+import child from './child'
+export default {
+  data () {
+    return {
+      Msg: ''
+    }
+  },
+  components: {
+    child
+  },
+  methods: {
+    getMsg (e) {
+      this.Msg = e
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
