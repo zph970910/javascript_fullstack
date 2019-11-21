ace8ea2e827498db6c39098737ff854946357d35
zph970910 <1076299371@qq.com>
2019-11-21 22:14:30 +0800

    11.21

---
+++ vue/vue-magic-music/src/vuex/modules/com.js
@@ -0,0 +1,38 @@
+import * as types from '../types'
+
+const state = {
+  showSidebar: false,
+  searchHistory: ['123', '24325465']
+}
+
+const mutations = {
+  [types.COM_SHOW_SIDE_BAR] (state, status) {
+    state.showSidebar = status
+  },
+  [types.COM_SAVE_SEARCH_HISTORY] (state, status) {
+    state.searchHistory = state
+  }
+}
+
+const actions = {
+  setShowSidebar ({commit}, status) {
+    commit(types.COM_SHOW_SIDE_BAR, status)
+  },
+  saveSearchHistory ({commit, state}, query) {
+    let searchHistory = [query, ...state.searchHistory.slice()]
+    searchHistory = [...new Set(searchHistory)]
+    commit(types.COM_SAVE_SEARCH_HISTORY, searchHistory)
+  }
+}
+
+const getters = {
+  showSidebar: state => state.showSidebar,
+  searchHistory: state => state.searchHistory
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
