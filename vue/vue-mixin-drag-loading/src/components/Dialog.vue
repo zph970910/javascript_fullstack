626c6c610ee7bfaa0a4ec73b4a3ad6273289ad9e
zph970910 <1076299371@qq.com>
2019-12-03 16:22:42 +0800

    mixin

---
+++ vue/vue-mixin-drag-loading/src/components/Dialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span @mousedown="start" style="position:fixed;top:0;left:0">这是一个拖拽的文本</span>
+    <el-button @click="open" >open</el-button>
+    <el-dialog title="tips" :visible="visible">
+      <span @click="close">这是一段信息</span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import dragAbleMix from './drag'
+export default {
+  mixins: [dragAbleMix],
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    open() {
+      this.visible = true
+    },
+    close() {
+      this.visible = false
+    }
+  },
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
