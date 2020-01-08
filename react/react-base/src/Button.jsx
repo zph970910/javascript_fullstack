4266b26aec7b694a26c578992eb9864140808707
zph970910 <1076299371@qq.com>
2020-01-08 12:04:40 +0800

    react

---
+++ react/react-base/src/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Button(props) {
+  let date = new Date().getTime()
+  // 通知父组件
+  const handleClick = () => {
+    console.log(123456)
+  }
+  const { type, children, receiveFromButton } = props
+  // 父组件传递一个回调函数过来，子组件通过参数通知父组件
+  receiveFromButton(date)
+
+  return (
+    <button onClick={handleClick}
+    className={type === 'primary' && 'primary-class'}
+    >{children}</button>
+  )
+}
+export default Button
