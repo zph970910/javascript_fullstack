1b9f4a91b1e450ea2e90a4ddf15042591fb6169a
zph970910 <1076299371@qq.com>
2020-01-14 22:54:29 +0800

    1

--- leetcode/remove_duplicates_sorted_list/1.js
+++ leetcode/remove_duplicates_sorted_list/1.js
@@ -1,7 +1,7 @@
 // 删除有序数组的重复项
 function removeDuolicates(nums) {
-  let slowP = 0
-  for(let fastP = 0; fastP < nums.length; fastP++) {
+  let slowP = 0 //慢指针
+  for(let fastP = 0; fastP < nums.length; fastP++) { // 快指针
     if (nums[slowP] != nums[fastP]) {
       slowP ++ 
       nums[slowP] = nums[fastP]
